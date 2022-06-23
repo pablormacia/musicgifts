8ad615cde2348f7316557b4a2e0283b8254e4913
pablormacia <pablo-macia@live.com.ar>
2022-06-23 09:10:52 -0300

    Working CartWidget & ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 
 function App() {
   return (
     <>
       <NavBar />
+      <ItemListContainer greeting="Bienvenidos" />
     </>
   );
 }
